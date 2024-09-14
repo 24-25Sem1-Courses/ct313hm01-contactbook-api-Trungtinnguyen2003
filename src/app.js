b42a95a23a8f6c189e0bfd73c3c779e67ea3703f
trungtinnguyen <tinb2105689@student.ctu.edu.vn>
2024-09-15 00:11:08 +0700

    Implement error handlers

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+
 const JSend = require('./jsend');
 const contactsRouter = require('./routes/contacts.router');
+const{
+    resourceNotFound,
+    handleError,
+} = require('./controllers/errors.controller')
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
 return res.json(JSend.success());
 });
+
 contactsRouter.setup(app);
+
+//handle 404 respone 
+app.use(resourceNotFound);
+
+//Define error
+app.use(handleError);
+
 module.exports = app;
